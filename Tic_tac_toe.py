c10d62c6e4be4204e718e7d555f11717fbc25bc0
ashwinibarapatre28 <ashwinibarapatre121@gmail.com>
2026-01-31 20:25:47 +0530

    Add files via upload

---
+++ Tic_tac_toe.py
@@ -0,0 +1,63 @@
+board = [" "] * 9
+def print_board():
+    print()
+    print(f" {board[0]} | {board[1]} | {board[2]} ")
+    print("---+---+---")
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
+    print("---+---+---")
+    print(f" {board[6]} | {board[7]} | {board[8]} ")
+    print()
+
+def check_winner(player):
+    win_conditions = [
+        (0,1,2), (3,4,5), (6,7,8),  
+        (0,3,6), (1,4,7), (2,5,8),  
+        (0,4,8), (2,4,6)            
+    ]
+    for a, b, c in win_conditions:
+        if board[a] == board[b] == board[c] == player:
+            return True
+    return False
+
+def is_draw():
+    return " " not in board
+
+current_player = "X"
+
+print("Welcome to Tic Tac Toe!")
+print("Positions are numbered 1 to 9 as shown below:")
+print("""
+ 1 | 2 | 3
+---+---+---
+ 4 | 5 | 6
+---+---+---
+ 7 | 8 | 9
+""")
+
+while True:
+    print_board()
+    move = input(f"Player {current_player}, choose a position (1-9): ")
+
+    if not move.isdigit() or not (1 <= int(move) <= 9):
+        print("Invalid input. Try again.")
+        continue
+
+    move = int(move) - 1
+
+    if board[move] != " ":
+        print("That place is already taken. Try again.")
+        continue
+
+    board[move] = current_player
+
+    if check_winner(current_player):
+        print_board()
+        print(f" Player {current_player} wins!")
+        break
+
+    if is_draw():
+        print_board()
+        print("It's a draw!")
+        break
+
+    current_player = "O" if current_player == "X" else "X"
